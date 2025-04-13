8769a1baf18f0b245c173460f880f3da91967977
YIP YU ZHE <yipyuzhe1402@gamil.com>
2025-04-13 16:57:56 +0800

    user registration or sign in done

--- website/app.py
+++ website/app.py
@@ -1,41 +1,51 @@
-from flask import Flask, render_template, request, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import  generate_passeord_hash
+from flask import Flask, render_template, request, redirect, flash 
+from flask_sqlalchemy import SQLAlchemy 
+from werkzeug.security import generate_password_hash
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.secret_key = 'ForMchat1234'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.string(80))
-    email = db.Column(db.string(120))
-    password = db.Column(db.string(200))
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 with app.app_context():
-    cd.create_all()
+    db.create_all()
+
+@app.route('/')
+def home():
+    return redirect('/register')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            flash('Passwords do not match!')
+            return redirect('/register')
 
         existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             flash('Email already registered.')
-            return redirect('/regiter')
+            return redirect('/register')
 
-        new_user =User(email=email, password=password)
+        new_user =User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
 
         flash('Registered successfully.')
         return redirect('/register')
 
-    return render_template('/regiter.html')
+    return render_template('register.html')
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
