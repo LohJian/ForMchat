78dabc9592e0239d4b73f2fcb6b810c00602d7dc
LohJian <zogmin925@gmail.com>
2025-04-22 19:04:03 +0800

    Clean up removed files

--- backend/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required
-from flask_cors import CORS
